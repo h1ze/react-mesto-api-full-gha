d1972d19d4829043c91034a6b46b4ca0623a7435
h1ze <burgov@mail.ru>
2023-05-21 12:17:57 +0400

    fix: исправить аргументы в Card на фронте

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,10 +4,10 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 function Card({card, onCardClick, onCardLike, onCardDelete}) {
 
     const currentUser = React.useContext(CurrentUserContext); 
-    const isOwn = card.owner._id === currentUser._id;
+    const isOwn = card.owner === currentUser._id;
 
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(userID => userID === currentUser._id);
 
     // Создаём переменную, которую после зададим в `className` для кнопки лайка
     const cardLikeButtonClassName = ( 
